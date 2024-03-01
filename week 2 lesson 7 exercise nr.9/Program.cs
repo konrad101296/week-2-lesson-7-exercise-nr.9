c02dfd31c590da11803e3f5e96aa0be1a079ec31
konrad101296 <konradslimak@o2.pl>
2024-03-01 16:22:25 +0100

    Add project files.

---
+++ week 2 lesson 7 exercise nr.9/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("podaj temperature:");
+double temp = double.Parse(Console.ReadLine());
+
+if (temp < 0)
+{
+    Console.WriteLine("cholernie pizdzi");
+}
+else if (temp >= 0 && temp < 10)
+{
+    Console.WriteLine("zimno");
+}
+else if (temp >= 10 && temp < 20)
+{
+    Console.WriteLine("chłodno");
+}
+else if (temp >= 20 && temp < 30)
+{
+    Console.WriteLine("w sam raz");
+}
+else if (temp >= 30 && temp < 40)
+{
+    Console.WriteLine("zaczyna byc słabo,bo goraco");
+}
+else if (temp >= 40)
+{
+    Console.WriteLine("a weź wyprowadzam się na Alaskę");
+}
